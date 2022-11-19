ea2d0939d8332b755749e32ef09fdc79b0169ce5
hariirawan <hari31121993@gmail.com>
2022-11-19 19:54:33 +0700

    setup dan slicing

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        "70px": "70px",
+      },
+    },
+  },
+  plugins: [],
+};
